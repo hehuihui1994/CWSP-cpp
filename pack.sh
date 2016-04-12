b9e3bcbb3b7527c1e4cb3a12749c2842e01a6823
heshenghuan <heshenghuan@sina.com>
2016-04-12 17:42:30 +0800

    Added package script file

---
+++ pack.sh
@@ -0,0 +1,15 @@
+FILES="README.md makefile pack.sh convert.cpp cwsp_*.cpp src/* include/*
+data/binarySource model/Dict"
+if [ $# -eq 0 ]
+then
+    echo "Please input version numbers of cwsp to package."
+    exit
+elif [ $# -eq 1 ]
+then
+    VERSION="$@"
+    echo "Version number:"$VERSION
+    echo "tar -czvf cwsp"$VERSION" README.md makefile pack.sh convert.cpp cwsp_*.cpp src/* include/* data/* model/*"
+    tar -czvf cwsp${VERSION}.tar $FILES
+else
+    echo "Too much parameters, just input version number after script."
+fi
