f2465b03a81035cd7ef1ab6ddb2fa9255a586a4f
heshenghuan <heshenghuan@sina.com>
2016-04-19 17:14:00 +0800

    Modified version

--- include/Config.h
+++ include/Config.h
@@ -37,8 +37,8 @@ using namespace std;
 
 namespace cwsp
 {
-    const string g_copyright = "\ncwsp: Chinese Word Segmentation Toolkit used multi-perceptron.\nCopyright (C) 2016. Version: 1.0.0, Last update: 2016-04-07\n";
-    const string g_version = "\nVersion: 1.0.0, Last update: 2016-04-07\n";
+    const string g_copyright = "\ncwsp: Chinese Word Segmentation Toolkit used multi-perceptron.\nCopyright (C) 2016. Version: 1.0.1, Last update: 2016-04-19\n";
+    const string g_version = "\nVersion: 1.0.1, Last update: 2016-04-19\n";
 
     const string g_Model_Header = "#bin,model";
     const int g_Header_Len = (int)g_Model_Header.length();
