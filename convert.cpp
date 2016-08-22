ecbd0ea768da3a4c9a73cd4a670e32420ab95dca
heshenghuan <heshenghuan@sina.com>
2016-08-22 16:59:42 +0800

    reprogramming convert

--- convert.cpp
+++ convert.cpp
@@ -18,53 +18,134 @@
 #include "Config.h"
 using namespace std;
 
+void print_help() {
+    cout << cwsp::g_copyright
+        << "\n\nNAME\n"
+        << "    convert -- convert model & src file of CWSP from text to binary file"
+        << "USAGE: "
+        << "    convert [-Ac][-dfpm file]\n"
+        << "OPTIONS:\n"
+        << "     -h    Print help infomation\n\n"
+        << "     -A    Convert all the model & src file with default path. Include: \n"
+        << "           CharType(./data/*), Feat(./model/Feat), Prob(./model/Prob), Dict"
+        << "           (./model/Dict) and Model(./model/Model). And also you can use "
+        << "           following command [-dfpm file] to give a specified file.\n\n"
+        << "     -c    Convert all the CharType files under './data/'\n\n"
+        << "     -d    Convert Dict file from the specified file(default: ./model/Dict)\n\n"
+        << "     -f    Convert Feat file from the specified file(default: ./model/Feat)\n\n"
+        << "     -p    Convert Prob file from the specified file(default: ./model/Prob)\n\n"
+        << "     -m    Convert MultiPerceptron Model file from the specified file(default:"
+        << "           ./model/Model)\n"
+        << endl;
+}
+
+void read_parameters(int argc, char *argv[], bool *All, bool *c, bool *d,
+                     string &dictfile, bool *f, string &featfile, bool *p,
+                     string &probfile, bool *m, string &modelfile) {
+    int i;
+    for (i = 1; (i<argc) && (argv[i])[0]=='-'; i++) {
+        switch ((argv[i])[1]) {
+            case 'h':
+                print_help();
+                exit(0);
+            case 'A':
+                *All = true;
+                break;
+            case 'c':
+                *c = true;
+                break;
+            case 'd':
+                *d = true;
+                if(i+1<argc && argv[i+1][0] != '-') {
+                    dictfile.clear();
+                    dictfile = string(argv[++i]);
+                }
+                break;
+            case 'f':
+                *f = true;
+                if(i+1<argc && argv[i+1][0] != '-') {
+                    featfile.clear();
+                    featfile = string(argv[++i]);
+                }
+                break;
+            case 'p':
+                *p = true;
+                if(i+1<argc && argv[i+1][0] != '-') {
+                    probfile.clear();
+                    probfile = string(argv[++i]);
+                }
+                break;
+            case 'm':
+                *m = true;
+                if(i+1<argc && argv[i+1][0] != '-') {
+                    modelfile.clear();
+                    modelfile = string(argv[++i]);
+                }
+                break;
+            default:
+                cout << "Unrecognized option: " << argv[i] << "!" << endl;
+                print_help();
+                exit(0);
+        }
+    }
+}
+
+bool CharTypeConvert(){
+    cwsp::CharType myCharType;
+    return myCharType.Convert2Binary();
+}
+
 int main(int argc, char *argv[])
 {
     cerr << cwsp::g_copyright << endl;
 
 #ifdef WIN32
-    string _datapath = "model\\";
+    string _modelpath = "model\\";
 #else
-    string _datapath = "model/";
+    string _modelpath = "model/";
 #endif
 
-    cwsp::CharType myCharType;
-    myCharType.Convert2Binary();
+    // cwsp::CharType myCharType;
+    // myCharType.Convert2Binary();
 
-    string featfile = _datapath + "Feat";
-    string probfile = _datapath + "Prob";
-    string dictfile = _datapath + "Dict";
-    string binaryFeat = _datapath + "Feat.bin";
-    string binaryProb = _datapath + "Prob.bin";
-    string binaryDict = _datapath + "Dict.bin";
+    string featfile = _modelpath + "Feat";
+    string probfile = _modelpath + "Prob";
+    string dictfile = _modelpath + "Dict";
+    string modelfile = _modelpath + "Model";
+    string binaryFeat = _modelpath + "Feat.bin";
+    string binaryProb = _modelpath + "Prob.bin";
+    string binaryDict = _modelpath + "Dict.bin";
+    string binaryModel = _modelpath + "Model.bin";
 
-    cerr << "Convert txt to binary......\n" << endl;
-    cwsp::SegFeat myFeat;
-    cwsp::SegProb myProb;
-    cwsp::SegDict myDict;
-    if(myFeat.ConvertToBinaryFile(featfile.c_str(), binaryFeat.c_str())){
-        cerr << "Feature file convertion is finished.\n" << endl;
-    }
-    else
-    {
-        cerr << "Error! Failed to convert Feature file to binary.\n"<<endl;
-    }
+    bool A = false,c = false,d = false,f = false,p = false,m = false;
 
-    if(myProb.ConvertToBinaryFile(probfile.c_str(), binaryProb.c_str())){
-        cerr << "Probability file convertion is finished.\n" << endl;
-    }
-    else
-    {
-        cerr << "Error! Failed to convert Probability file to binary.\n"<<endl;
-    }
+    // cerr << "Convert txt to binary......\n" << endl;
+    // cwsp::SegFeat myFeat;
+    // cwsp::SegProb myProb;
+    // cwsp::SegDict myDict;
+    // if(myFeat.ConvertToBinaryFile(featfile.c_str(), binaryFeat.c_str())){
+    //     cerr << "Feature file conversion is finished.\n" << endl;
+    // }
+    // else
+    // {
+    //     cerr << "Error! Failed to convert Feature file to binary.\n"<<endl;
+    // }
 
-    if(myDict.ConvertToBinaryFile(dictfile.c_str(), binaryDict.c_str())){
-        cerr << "Dictionary file convertion is finished.\n" << endl;
-    }
-    else
-    {
-        cerr << "Error! Failed to convert Dictionary file to binary.\n"<<endl;
-    }
-    cerr << "\n\nConvertion is finished." << endl;
+    // if(myProb.ConvertToBinaryFile(probfile.c_str(), binaryProb.c_str())){
+    //     cerr << "Probability file conversion is finished.\n" << endl;
+    // }
+    // else
+    // {
+    //     cerr << "Error! Failed to convert Probability file to binary.\n"<<endl;
+    // }
+
+    // if(myDict.ConvertToBinaryFile(dictfile.c_str(), binaryDict.c_str())){
+    //     cerr << "Dictionary file conversion is finished.\n" << endl;
+    // }
+    // else
+    // {
+    //     cerr << "Error! Failed to convert Dictionary file to binary.\n"<<endl;
+    // }
+    // cerr << "\n\nConversion is finished." << endl;
     return 0;
 }
